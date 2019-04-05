8f352fb8e35dc905fdc4c8c17d117aa3001e2f2b
Annia Forney <anniaforney@gmail.com>
2019-04-05 02:24:41 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -2,3 +2,9 @@
 $('img').on('load', function(){
   //actions you want to happen
 });
+
+$(document).on('keypress', function(key) {
+  if (key.which == 13){
+    alert('enter was pressed');
+  }
+});
